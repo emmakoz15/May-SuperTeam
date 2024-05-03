c4b093a9d168051626c2112b0bf835a60fd5f9d6
anisenson <adamjn524@gmail.com>
2024-05-03 11:53:40 -0400

    Finished Demo ?

--- script.js
+++ script.js
@@ -9,11 +9,19 @@ const teamMembers = [
     },
     {
         name: 'Lionel Messi Jr OOHHHH',
+        position: 'midfield',
+        skills: ['dribbling', 'defending', 'shooting'],
+        strengths: 'Young',
+        weaknesses: 'SHORT',
+        bio: "BIO FOR MESSI JR",
+    },
+    {
+        name: 'RONALDO',
         position: 'Forward',
-        skills: ['dribbling', 'passing', 'shooting'],
-        strengths: 'ball control',
-        weaknesses: 'heading ability',
-        bio: "BLAH BLAH BLAH YAP YAP",
+        skills: ['GOOD', 'Scoring', 'shooting'],
+        strengths: 'BIG',
+        weaknesses: 'NO Englsh',
+        bio: "This is Filler Text",
     },
 ]
 
@@ -24,10 +32,23 @@ function generateTeamCards(){
         const card = document.createElement('div')
         card.classList.add('col-md-4')
 
+        //Create a list of skills with the <li> tag
+        const skillsList = member.skills.map(skill => `<li> ${skill} </li>`).join('')
+
         card.innerHTML = `
         <div class = "card">
-        <div class = "card-header"> ${member.name}</div>
-        <div class = "card-body">
+            <div class = "card-header"> 
+                ${member.name}
+            </div>
+            
+            <div class = "card-body">
+                <p><b>Position: </b> ${member.position}</p>
+                <p><b>Skills:</b><ul>${skillsList}</ul>                <p><b>Strengths: </b>${member.strengths}</p>
+                <p><b>Weaknesses: </b> ${member.weaknesses}</p>
+                <p><b>bio: </b>${member.bio}</p>
+
+            </div>
+        </div>
         `
 
         teamCardsContainer.appendChild(card)
